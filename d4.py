3183e1ada0583c1b489dee839a186b30d821ff9e
peturjokull <pth20@hi.is>
2020-03-25 18:19:31 +0000

    dæmi 4 fáum ekki alveg sömu tölur og lausnin

--- d4.py
+++ d4.py
@@ -1,118 +1,106 @@
 import math
 import numpy as np
-def daemi4(r0,r1,r2,r3,r4,c,rho,theta,phi):
+import numpy.linalg as lin
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-	# A= {'a' : r1['x'], 'b' : r2['x'], 'c' : r3['x'], 'd' : r4['x']}
-	# B= {'a' : r1['y'], 'b' : r2['y'], 'c' : r3['y'], 'd' : r4['y']}
-	# C= {'a' : r1['z'], 'b' : r2['z'], 'c' : r3['z'], 'd' : r4['z']}
-	# t= {'a' : r1['d'], 'b' : r2['d'], 'c' : r3['d'], 'd' : r4['d']}
-    A = np.asarray([r1['x'],r2['x'],r4['x'],r4['x']])
-    B = np.asarray([r1['y'],r2['y'],r4['y'],r4['y']])
-    C = np.asarray([r1['z'],r2['z'],r4['z'],r4['z']])
-    t = np.asarray([r1['d'],r2['d'],r4['d'],r4['d']])
-    ###
-	R=np.zeros(4)
-	t_m=np.zeros(4)
-    ####
+c = 299792.458  # Ljoshradi km/s
+r1 = [15600, 7540, 20140, 0.07074]  # Gervitungl 1
+r2 = [18760, 2750, 18610, 0.07220]  # Gervitungl 2
+r3 = [17610, 14630, 13480, 0.07690]  # Gervitungl 3
+r4 = [19170, 610, 18390, 0.07242]  # Gervitungl 4
+r0 = [0, 0, 6370, 0.0001]  # hnit mottakara
+rho = 26570  # km
+phi = [0, math.pi/8, math.pi/4, math.pi/4]
+theta = [0, math.pi/2, math.pi, 3*math.pi/2]
+R = np.zeros(4)
+t_m = np.zeros(4)
+A = [r1[0], r2[0], r3[0], r4[0]]
+B = [r1[0], r2[1], r3[2], r4[3]]
+C = [r1[0], r2[1], r3[2], r4[3]]
+t_m = [r1[3], r2[3], r3[3], r4[3]]
+
+for i in range(4):
+    A[i] = rho*math.cos(phi[i])*math.cos(theta[i])
+    B[i] = rho*math.cos(phi[i])*math.sin(theta[i])
+    C[i] = rho*math.cos(phi[i])
+    R[i] = math.sqrt(A[i]**2 + B[i]**2 + (C[i]-r0[3])**2)
+    t_m[i] = r0[3] + R[i]/c
+print('A',A, '\n', 'B',B, '\n', 'C',C, '\n', 'R',R)
+teikna = False
+if teikna is True:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(r0[0], r0[1], r0[2], c='b', s=1000)
+    ax.scatter(A[0], A[1], A[2], c='r')
+    ax.scatter(B[0], B[1], B[2], c='r')
+    ax.scatter(C[0], C[1], C[2], c='r')
+    ax.scatter(R[0], R[1], R[2], c='r')
+    # ax.scatter(19170,610,18390,c='r')
+    ax.set_xlabel('x-ás (km)')
+    ax.set_ylabel('y-ás (km)')
+    ax.set_zlabel('z-ás (km)')
+
+    ax.invert_xaxis()
+
+    plt.show()
+
+dt = 10**-8  # nákvæmni klukku í gervihnöttum
+errorcoef = [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 0, 1, 1], [0, 1, 0, 0], [0, 1, 0, 1], [0, 1, 1, 0], [
+    0, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 1], [1, 0, 1, 0], [1, 0, 1, 1], [1, 1, 0, 0], [1, 1, 0, 1], [1, 1, 1, 0], [1, 1, 1, 1]]
+errorcoef = np.array(errorcoef)
+errormagcoef = 0
+maxFE = 0
+
+for i in range(0, 16):
+
+    t_new = [
+        t_m[0] + dt*errorcoef[i, 0],
+        t_m[1] + dt*errorcoef[i, 1],
+        t_m[2] + dt*errorcoef[i, 2],
+        t_m[3] + dt*errorcoef[i, 3]
+    ]
+
+    for j in range(0, 10):
+        DF = np.array([
+            [(r0[0] - A[0])*2, (r0[1] - B[0])*2,
+                (r0[2] - C[0])*2, (2*c**2)*(t_new[0] - r0[3])],
+            [(r0[0] - A[1])*2, (r0[1] - B[1])*2,
+                (r0[2] - C[1])*2, (2*c**2)*(t_new[1] - r0[3])],
+            [(r0[0] - A[2])*2, (r0[1] - B[2])*2,
+                (r0[2] - C[2])*2, (2*c**2)*(t_new[2] - r0[3])],
+            [(r0[0] - A[3])*2, (r0[1] - B[3])*2,
+                (r0[2] - C[3])*2, (2*c**2)*(t_new[3] - r0[3])]
+        ])
+
+        F = np.array([
+        [(r0[0] - A[0])**2 + (r0[1] - B[0])**2 +
+            (r0[2] - C[0])**2 - (c**2)*(t_new[0] - r0[3])**2],
+        [(r0[0] - A[1])**2 + (r0[1] - B[1])**2 +
+            (r0[2] - C[1])**2 - (c**2)*(t_new[1] - r0[3])**2],
+        [(r0[0] - A[2])**2 + (r0[1] - B[2])**2 +
+            (r0[2] - C[2])**2 - (c**2)*(t_new[2] - r0[3])**2],
+        [(r0[0] - A[3])**2 + (r0[1] - B[3])**2 +
+            (r0[2] - C[3])**2 - (c**2)*(t_new[3] - r0[3])**2]
+        ])
 
 
-	# A['a']=rho*math.cos(phi['a'])*math.cos(theta['a'])
-	# B['a']=rho*math.cos(phi['a'])*math.sin(theta['a'])
-	# C['a']=rho*math.sin(phi['a'])
-    A=[r1[0],r2[0],r3[0],r4[0]]
-    B=[r1[1],r2[1],r3[1],r4[1]]
-    C=[r1[2],r2[2],r3[2],r4[2]]
-    t_m = np.asarray(r1[4],r2[4],r3[4],r4[4])
-    for i in range(5):
-        A[i] = rho*math.cos(phi[i])*math.cos(theta[i])
-        B[i] = rho*math.cos(phi[i])*math.cos(theta[i])
-        C[i] = rho*math.cos(phi[i])*math.cos(theta[i])
-        R[i] = rho*math.cos(phi[i])*math.cos(theta[i])
-        t_m[i]=r0[4] + R[i]/c
-    return A
+        res = lin.solve(DF, F)
+        for i in range(len(r0)):
+            r0[i] = r0[i] - res[i,0]
 
-	# R[0]=math.sqrt(A['a']**2 + B['a']**2 + (C['a']-r0['z'])**2)
-	# t['a']=r0['z'] + R[0]/c
 
-	# A['b']=rho*math.cos(phi['b'])*math.cos(theta['b'])
-	# B['b']=rho*math.cos(phi['b'])*math.sin(theta['b'])
-	# C['b']=rho*math.sin(phi['b'])
-	# R[1]=math.sqrt(A['b']**2 + B['b']**2 + (C['b']-r0['z'])**2)
-	# t['b']=r0['z'] + R[1]/c
+    forwarderror = lin.norm(np.asarray([r0[0], r0[1], r0[2]-6370]), ord=2)
+    backwarderror = c*lin.norm([t_new[0]-t_m[0], t_new[1] -
+                                t_m[1], t_new[2]-t_m[2], t_new[3]-t_m[3]], ord=2)
 
-	# A['c']=rho*math.cos(phi['c'])*math.cos(theta['c'])
-	# B['c']=rho*math.cos(phi['c'])*math.sin(theta['c'])
-	# C['c']=rho*math.sin(phi['c'])
-	# R[2]=math.sqrt(A['c']**2 + B['c']**2 + (C['c']-r0['z'])**2)
-	# t['c']=r0['z'] + R[2]/c
+    if maxFE <= np.absolute(forwarderror):
+        maxFE = np.absolute(forwarderror)
 
-	# A['d']=rho*math.cos(phi['d'])*math.cos(theta['d'])
-	# B['d']=rho*math.cos(phi['d'])*math.sin(theta['d'])
-	# C['d']=rho*math.sin(phi['d'])
-	# R[3]=math.sqrt(A['d']**2 + B['d']**2 + (C['d']-r0['z'])**2)
-	# t['d']=r0['z'] + R[3]/c
+if errormagcoef <= np.absolute(forwarderror/backwarderror):
+    errormagcoef = np.absolute(forwarderror/backwarderror)
 
-	# dt=10**-8 #nákvæmni klukku í gervihnöttum
-	# errorcoef=[[0,0,0,0],[0,0,0,1],[0,0,1,0],[0,0,1,1],[0,1,0,0],[0,1,0,1],[0,1,1,0],[0,1,1,1],[1,0,0,0],[1,0,0,1],[1,0,1,0],[1,0,1,1],[1,1,0,0],[1,1,0,1],[1,1,1,0],[1,1,1,1]]
-	# errorcoef=np.array(errorcoef)
-	# errormagcoef = 0
-	# maxFE = 0
-
-	# for i in range(0,15):
-	# 	#t_new = t + np.multiply(dt,errorcoef[i])
-	# 	t_new = t + dt*errorcoef[i]
-	# 	for j in range(0,9):
-	# 		DF = np.array([
-	# 		[(r0[0] - A[0])*2 , (r0[1] - B[0])*2 , (r0[2] - C[0])*2 , (2*c**2)*(t_new[0]- r0[3])],
-	# 		[(r0[0] - A[1])*2 , (r0[1] - B[1])*2 , (r0[2] - C[1])*2 , (2*c**2)*(t_new[1] - r0[3])],
-	# 		[(r0[0] - A[2])*2 , (r0[1] - B[2])*2 , (r0[2] - C[2])*2 , (2*c**2)*(t_new[2]- r0[3])],
-	# 		[(r0[0] - A[3])*2 , (r0[1] - B[3])*2 , (r0[2] - C[3])*2 , (2*c**2)*(t_new[3] - r0[3])]
-	# 		])
-
-	# 		F = np.array([
-	# 		[(r0[0] - A[0])**2 + (r0[1] - B[0])**2 + (r0[2] -  C[0])**2 - (c*(t_new[0] - r0[3]))**2],
-	# 		[(r0[0] - A[1])**2 + (r0[1] - B[1])**2 + (r0[2] -  C[1])**2 - (c*(t_new[0] - r0[3]))**2],
-	# 		[(r0[0] - A[2])**2 + (r0[1] - B[2])**2 + (r0[2] -  C[2])**2 - (c*(t_new[0] - r0[3]))**2],
-	# 		[(r0[0] - A[3])**2 + (r0[1] - B[3])**2 + (r0[2] -  C[3])**2 - (c*(t_new[0] - r0[3]))**2]
-	# 		])
-
-	# 		res=lin.solve(DF,F)
-	# 		res = lin.solve(DF,F)
-	# 	r0['x'] = r0['x'] - res[0][0]
-	# 	r0['y'] = r0['y'] - res[1][0]
-	# 	r0['z'] = r0['z'] - res[2][0]
-	# 	r0['d'] = r0['d'] - res[3][0]
-
-	# 	forwarderror=lin.norm([r0['x'],r0['y'],r0['z']-6370],np.inf)
-	# 	backwarderror=c*lin.norm(t_new-t,np.inf)
-
-	# 	if maxFE <= abs(forwarderror):
-	# 		maxFE = abs(forwarderror)
-
-	# 	if errormagcoef <= abs(forwarderror/backwarderror):
-	# 		errorcoeff = abs(forwarderror/backwarderror)
-	# #return 1,2
-	# return errorcoeff,maxFE	
-
-# r1 = {'x':15600, 'y':7540 ,'z':20140,'d':0.07074 } #Gervitungl 1
-# r2 = {'x':18760, 'y':2750 ,'z':18610,'d':0.07220 } #Gervitungl 2
-# r3 = {'x':17610, 'y':14630,'z':13480,'d':0.07690 } #Gervitungl 3
-# r4 = {'x':19170, 'y':610  ,'z':18390,'d':0.07242 } #Gervitungl 4
-# r0 = {'x':0,'y':0,'z':6370,'d':0} #upphafsvigur
-# r02 = {'x':0,'y':0,'z':6370,'d':0.0001} #upphafsvigur2
-# phi = {'a':0, 'b': math.pi/8,'c': math.pi/4, 'd': math.pi/4}
-# theta = {'a':0, 'b': math.pi/2,'c': math.pi, 'd': 3*math.pi/2}
-# rho = 26570 #km
-# c=299792.458 #ljóshraði km/s
-# print(daemi4(r0,r1,r2,r3,r4,c,rho,theta,phi))
-
-c=299792.458; #Ljoshradi km/s
-r1=[15600,7540,20140,0.07074]; #Gervitungl 1
-
-   
-r2=[18760,2750,18610,0.07220] #Gervitungl 2
-r3=[17610,14630,13480,0.07690] #Gervitungl 3
-r4=[19170,610,18390,0.07242] #Gervitungl 4
-r0=[0,0,6370,0.0001] #hnit mottakara
-rho = 26570 #km
-print(daemi4(r0,r1,r2,r3,r4,c,rho,theta,phi))
+print("error magniﬁcation factor")
+print(errormagcoef)
+print("max distance error")
+print(maxFE)
