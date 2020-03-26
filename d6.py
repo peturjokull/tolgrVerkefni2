5c338ccf9b652f7b50cea07011729b8a267739cb
peturjokull <pth20@hi.is>
2020-03-26 12:29:42 +0000

    fylki R

--- d6.py
+++ d6.py
@@ -1,5 +1,12 @@
-from d4 import test
+import numpy as np
 c = 299792.458  # Ljoshradi km/s
 r0 = [0, 0, 6370, 0.0001]  # hnit mottakara
 rho = 26570  # km
-print(d4.test())
+
+# Aðferð til að fá hnit dreifð jafnt yfir jörðina
+def sample_spherical(npoints, ndim=3):
+    vec = np.random.randn(ndim, npoints)
+    vec /= np.linalg.norm(vec, axis=0)
+    return vec.T
+
+R = sample_spherical(8)
